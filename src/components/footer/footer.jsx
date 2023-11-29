270023a7d180cdb6fd12b858f14504bcd2240860
bruno-robles404 <roblesbruno76@gmail.com>
2023-11-29 11:42:02 -0300

    footer terminado

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -15,13 +15,17 @@ const Footer = () => {
     <footer className="footer-container">
 
       <div className="footer-green">
-        <p> En Bariloche se generan 280 toneladas de basura por día.
+        <p className="green-paragraph"> En Bariloche se generan 280 toneladas de basura por día.
           En verano, estos residuos se incrementan un 30% y generan
           70 toneladas más por día.
           Por eso, con residuos reciclados hicimos este souvenir.
-          </p>
-          
-
+        </p>
+        <h2 className="green-title">¿POR QUÉ UN SOUVENIR? </h2>
+        <p className="green-paragraph">
+          Para que tomemos conciencia y nos recuerde la importancia de ayudar
+          a que la basura no quede en la naturaleza de Bariloche.
+          Con lo recaudado se organizarán nuevas recolecciones de basura en espacios naturales.
+        </p>
       </div>
 
       <div className="footer-brand">
@@ -33,21 +37,22 @@ const Footer = () => {
 
           <li className="list-item">
             <a className="item-link" href=""><FontAwesomeIcon icon={faFacebook} /> </a>
-            <a href=""><p className="item-paragraph">DANOS UN LIKE EN FACEBOOK</p></a>
+            <a className="item-link" href=""><p className="item-paragraph">DANOS UN LIKE EN FACEBOOK</p></a>
           </li>
 
           <li className="list-item" >
             <a className="item-link" href="">  <FontAwesomeIcon icon={faInstagram} /></a>
-            <a href=""><p className="item-paragraph">UNETE A NUESTRO INSTAGRAM</p></a>
+            <a className="item-link" href=""><p className="item-paragraph">UNETE A NUESTRO INSTAGRAM</p></a>
           </li>
 
           <li className="list-item">
             <a className="item-link" href=""><FontAwesomeIcon icon={faYoutube} /> </a>
-            <a href=""><p className="item-paragraph">VISITANOS EN YOUTUBE</p></a>
+            <a className="item-link" href=""><p className="item-paragraph">VISITANOS EN YOUTUBE</p></a>
           </li>
         </ul>
       </div>
 
+      
     </footer>
   );
 };
